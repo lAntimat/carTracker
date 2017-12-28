379260af3b9004856633677ef586b97981c96f45
lAntimat <tamitnal@gmail.com>
2017-12-28 09:38:15 +0300

    add loadTrack from RightTech (bug fix)

---
+++ app/src/main/java/com/tracker/lantimat/cartracker/mapActivity/API/TrackR.java
@@ -0,0 +1,220 @@
+package com.tracker.lantimat.cartracker.mapActivity.API;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Awesome Pojo Generator
+ */
+public class TrackR {
+    @SerializedName("driver_message")
+    @Expose
+    private String driver_message;
+    @SerializedName("ex_dig_out_1")
+    @Expose
+    private double ex_dig_out_1;
+    @SerializedName("ex_dig_out_5")
+    @Expose
+    private Integer ex_dig_out_5;
+    @SerializedName("ex_dig_out_4")
+    @Expose
+    private Integer ex_dig_out_4;
+    @SerializedName("lon")
+    @Expose
+    private double lon;
+    @SerializedName("ex_dig_out_3")
+    @Expose
+    private Integer ex_dig_out_3;
+    @SerializedName("ex_dig_out_2")
+    @Expose
+    private Integer ex_dig_out_2;
+    @SerializedName("ex_dig_out_9")
+    @Expose
+    private Integer ex_dig_out_9;
+    @SerializedName("ex_dig_out_8")
+    @Expose
+    private Integer ex_dig_out_8;
+    @SerializedName("egts")
+    @Expose
+    private Boolean egts;
+    @SerializedName("speed")
+    @Expose
+    private double speed;
+    @SerializedName("ex_dig_out_7")
+    @Expose
+    private Integer ex_dig_out_7;
+    @SerializedName("bat-voltage")
+    @Expose
+    private double batVoltage;
+    @SerializedName("ex_dig_out_6")
+    @Expose
+    private Integer ex_dig_out_6;
+    @SerializedName("gps_odometer")
+    @Expose
+    private double gps_odometer;
+    @SerializedName("real-time")
+    @Expose
+    private double realTime;
+    @SerializedName("angle")
+    @Expose
+    private double angle;
+    @SerializedName("id")
+    @Expose
+    private String id;
+    @SerializedName("lat")
+    @Expose
+    private double lat;
+    @SerializedName("ex_dig_in_2")
+    @Expose
+    private Integer ex_dig_in_2;
+    @SerializedName("height")
+    @Expose
+    private double height;
+    @SerializedName("ex_dig_in_1")
+    @Expose
+    private Integer ex_dig_in_1;
+    @SerializedName("mileage")
+    @Expose
+    private double mileage;
+    @SerializedName("glonass_inview")
+    @Expose
+    private double glonass_inview;
+    @SerializedName("ex_dig_out_10")
+    @Expose
+    private Integer ex_dig_out_10;
+    @SerializedName("ex_dig_out_11")
+    @Expose
+    private Integer ex_dig_out_11;
+    @SerializedName("voltage")
+    @Expose
+    private double voltage;
+    @SerializedName("gps_inview")
+    @Expose
+    private double gps_inview;
+    @SerializedName("_id")
+    @Expose
+    private String _id;
+    @SerializedName("time")
+    @Expose
+    private double time;
+
+    public String getDriver_message() {
+        return driver_message;
+    }
+
+    public double getEx_dig_out_1() {
+        return ex_dig_out_1;
+    }
+
+    public Integer getEx_dig_out_5() {
+        return ex_dig_out_5;
+    }
+
+    public Integer getEx_dig_out_4() {
+        return ex_dig_out_4;
+    }
+
+    public Integer getEx_dig_out_3() {
+        return ex_dig_out_3;
+    }
+
+    public Integer getEx_dig_out_2() {
+        return ex_dig_out_2;
+    }
+
+    public Integer getEx_dig_out_9() {
+        return ex_dig_out_9;
+    }
+
+    public Integer getEx_dig_out_8() {
+        return ex_dig_out_8;
+    }
+
+    public Boolean getEgts() {
+        return egts;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public Integer getEx_dig_out_7() {
+        return ex_dig_out_7;
+    }
+
+    public double getBatVoltage() {
+        return batVoltage;
+    }
+
+    public Integer getEx_dig_out_6() {
+        return ex_dig_out_6;
+    }
+
+    public double getGps_odometer() {
+        return gps_odometer;
+    }
+
+    public double getRealTime() {
+        return realTime;
+    }
+
+    public double getAngle() {
+        return angle;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public double getLon() {
+        return lon;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public Integer getEx_dig_in_2() {
+        return ex_dig_in_2;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public Integer getEx_dig_in_1() {
+        return ex_dig_in_1;
+    }
+
+    public double getMileage() {
+        return mileage;
+    }
+
+    public double getGlonass_inview() {
+        return glonass_inview;
+    }
+
+    public Integer getEx_dig_out_10() {
+        return ex_dig_out_10;
+    }
+
+    public Integer getEx_dig_out_11() {
+        return ex_dig_out_11;
+    }
+
+    public double getVoltage() {
+        return voltage;
+    }
+
+    public double getGps_inview() {
+        return gps_inview;
+    }
+
+    public String get_id() {
+        return _id;
+    }
+
+    public double getTime() {
+        return time;
+    }
+}
